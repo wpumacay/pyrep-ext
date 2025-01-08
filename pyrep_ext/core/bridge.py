5705af383f233ff6e675ea41c23157ea6312114d
wpumacay <wpumacay@gmail.com>
2025-01-08 17:13:31 -0500

    (chore): applies some formatting

--- pyrep_ext/core/bridge.py
+++ pyrep_ext/core/bridge.py
@@ -2,29 +2,31 @@ import ctypes
 import functools
 import sys
 
-from .lib import cpllib, const
+from .lib import const, cpllib
 from .stack import read, write
+
 
 def load():
     # add coppeliaSim's pythondir to sys.path:
     pythonDirPtr = cpllib.simGetStringParam(const.sim_stringparam_pythondir)
-    pythonDir = ctypes.string_at(pythonDirPtr).decode('utf-8')
+    pythonDir = ctypes.string_at(pythonDirPtr).decode("utf-8")
     cpllib.simReleaseBuffer(pythonDirPtr)
     if pythonDir not in sys.path:
         sys.path.append(pythonDir)
 
     # load lua functions for call(), getObject(), etc...:
-    call('require', ('scriptClientBridge',))
+    call("require", ("scriptClientBridge",))
 
 
 @functools.cache
 def getTypeHints(func):
     # calltip comes from the CoppeliaSim python folder, added to sys.path
-    from calltip import FuncDef, VarArgs # type: ignore
-    c = call('sim.getApiInfo', [-1, func], (('int', 'string'), ('string')))
+    from calltip import FuncDef, VarArgs  # type: ignore
+
+    c = call("sim.getApiInfo", [-1, func], (("int", "string"), ("string")))
     if not c:
         return (None, None)
-    c = c.split('\n')[0]
+    c = c.split("\n")[0]
     fd = FuncDef.from_calltip(c)
     inArgs = list(fd.in_args)
     if inArgs and isinstance(inArgs[-1], VarArgs):
@@ -40,15 +42,15 @@ def call(func, args, typeHints=None):
         typeHints = getTypeHints(func)
     stackHandle = cpllib.simCreateStack()
     write(stackHandle, args, typeHints[0])
-    s = cpllib.simGetScriptHandleEx(const.sim_scripttype_sandbox, -1, None);
-    f = ctypes.c_char_p(f'{func}@lua'.encode('ascii'))
+    s = cpllib.simGetScriptHandleEx(const.sim_scripttype_sandbox, -1, None)
+    f = ctypes.c_char_p(f"{func}@lua".encode("ascii"))
     r = cpllib.simCallScriptFunctionEx(s, f, stackHandle)
     if r == -1:
         if False:
-            what = f'simCallScriptFunctionEx({s}, {func!r}, {args!r})'
+            what = f"simCallScriptFunctionEx({s}, {func!r}, {args!r})"
         else:
-            what = 'simCallScriptFunctionEx'
-        raise Exception(f'{what} returned -1')
+            what = "simCallScriptFunctionEx"
+        raise Exception(f"{what} returned -1")
     ret = read(stackHandle, typeHints[1])
     cpllib.simReleaseStack(stackHandle)
     if len(ret) == 1:
@@ -60,29 +62,36 @@ def call(func, args, typeHints=None):
 def getObject(name, _info=None):
     ret = type(name, (), {})
     if not _info:
-        _info = call('scriptClientBridge.info', [name])
-    for k, v in _info.items(): # type: ignore
+        _info = call("scriptClientBridge.info", [name])
+    for k, v in _info.items():  # type: ignore
         if not isinstance(v, dict):
-            raise ValueError('found nondict')
-        if len(v) == 1 and 'func' in v:
-            if f'{name}.{k}' == 'sim.getScriptFunctions':
-                setattr(ret, k, lambda scriptHandle:
-                        type('', (object,), {
-                            '__getattr__':
-                                lambda _, func:
-                                    lambda *args:
-                                        call('sim.callScriptFunction', (func, scriptHandle) + args)
-                        })())
+            raise ValueError("found nondict")
+        if len(v) == 1 and "func" in v:
+            if f"{name}.{k}" == "sim.getScriptFunctions":
+                setattr(
+                    ret,
+                    k,
+                    lambda scriptHandle: type(
+                        "",
+                        (object,),
+                        {
+                            "__getattr__": lambda _, func: lambda *args: call(
+                                "sim.callScriptFunction",
+                                (func, scriptHandle) + args,
+                            )
+                        },
+                    )(),
+                )
                 continue
-            setattr(ret, k, lambda *a, func=f'{name}.{k}': call(func, a))
-        elif len(v) == 1 and 'const' in v:
-            setattr(ret, k, v['const'])
+            setattr(ret, k, lambda *a, func=f"{name}.{k}": call(func, a))
+        elif len(v) == 1 and "const" in v:
+            setattr(ret, k, v["const"])
         else:
-            setattr(ret, k, getObject(f'{name}.{k}', _info=v))
+            setattr(ret, k, getObject(f"{name}.{k}", _info=v))
     return ret
 
 
 def require(obj):
-    call('scriptClientBridge.require', [obj])
+    call("scriptClientBridge.require", [obj])
     o = getObject(obj)
     return o
