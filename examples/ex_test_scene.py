92a6bec025a40df8590c15afbb75d5a2ac2cca81
wpumacay <wpumacay@gmail.com>
2025-01-10 11:17:34 -0500

    (feat): adds set_position method for objects

--- examples/ex_test_scene.py
+++ examples/ex_test_scene.py
@@ -1,4 +1,7 @@
+import time
 from pathlib import Path
+
+import numpy as np
 
 from pyrep_ext.objects.shape import Shape
 from pyrep_ext.pyrep import PyRep
@@ -16,8 +19,13 @@ def main() -> int:
     sphere = Shape("sphere")
 
     print(f"sphere handle: {sphere.get_handle()}")
+    print(f"sphere position: {sphere.get_position()}")
+
+    _, _, z = sphere.get_position()
 
     for _ in range(1000):
+        x, y = 0.5 * np.cos(time.time()), 0.5 * np.sin(time.time())
+        sphere.set_position([x, y, z])
         pr.step()
 
     pr.stop()
--- pyrep_ext/objects/object.py
+++ pyrep_ext/objects/object.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from typing import Any, Optional, Union
 
+import numpy as np
+
 from pyrep_ext.const import ObjectType
 from pyrep_ext.core.errors import WrongObjectTypeError
 from pyrep_ext.core.sim import SimBackend
@@ -58,3 +60,48 @@ class Object:
 
     def get_handle(self) -> int:
         return self._handle
+
+    def get_position(self, relative_to: Optional[Object] = None) -> np.ndarray:
+        """
+        Returns the position this object with respect to a given object. If no
+        relative object is given, then the position of this object is with
+        respect to the world frame.
+
+        Parameters
+        ----------
+            relative_to: Optional[Object]
+                A reference to an object used as reference frame
+
+        Returns
+        -------
+            np.ndarray
+                The position of this object measured respect to the given frame
+        """
+        rel_to_handle = -1 if relative_to is None else relative_to.get_handle()
+        position = self._sim_api.getObjectPosition(self._handle, rel_to_handle)
+        return np.array(position, dtype=np.float64)
+
+    def set_position(
+        self,
+        position: Union[list, np.ndarray],
+        relative_to: Optional[Object] = None,
+        reset_dynamics: bool = True,
+    ) -> None:
+        """
+        Sets the position of this object with respect to the given object. If no
+        object is given as reference, then the position is in world space.
+
+        Parameters
+        ----------
+            position: Union[list, np.ndarray]
+                The position of the object
+            relative_to: Optional[Object]
+                An object to be used as reference to set the position
+            reset_dynamics: bool
+                Whether or not to reset the dynamics after moving the object
+        """
+        rel_to_handle = -1 if relative_to is None else relative_to.get_handle()
+
+        self._sim_api.setObjectPosition(
+            self._handle, list(position), rel_to_handle
+        )
